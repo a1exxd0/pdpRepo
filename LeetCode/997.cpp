69c739c201badad0581f12c258fe51e027dd6354
Alex Do <alexxdo2004@gmail.com>
2024-02-22 10:22:47 +0000

    multithreaded easy

---
+++ LeetCode/997.cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<thread>
+#include<iostream>
+using namespace std;
+
+
+vector<int> inc(const vector<vector<int>> trust, const int n, const int z){
+    int tlen = trust.size();
+    vector<int> res(n+1, 0);
+    for(int i = 0; i < tlen; i++){
+        res[trust[i][z]]++;
+    }
+    return res;
+}
+
+class Solution {
+public:
+    int findJudge(const int n, vector<vector<int>>& trust) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+        vector<int> trusted;
+        std::thread th1([&trusted, trust, n](){
+            trusted = inc(trust, n, 1);
+        });
+
+        vector<int> trusting;
+        std::thread th2([&trusting, trust, n](){
+            trusting = inc(trust, n, 0);
+        });
+        
+        th1.join();
+        th2.join();
+
+        vector<int> candidates;
+        for (int i = 1; i <= n; i++){
+            if(trusted[i] == n-1){
+                if(trusting[i] == 0){
+                    candidates.push_back(i);
+                }
+            }
+        }
+        if (candidates.size() != 1) return -1;
+        else return candidates[0];
+        
+    }
+};
+
